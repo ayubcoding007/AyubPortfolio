0181df758c3904bf7276e82f7c6bb095c2c4e5f3
Ayub Sheikh <abupop007@gmail.com>
2024-12-26 10:38:08 +0530

    done!

---
+++ src/components/About.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+        Motivated  Final year student looking for a front-end web implementation position to leverage skills in HTML, CSS, JavaScript, Bootstrap and React.js. Used to contribute to a team and enhancing user experience through innovative web solutions. And I am learning Node.js and Express.js for backend.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+          [Bachelor of Computer Applications (BCA)], [Khwaja Moinuddin Chishti Language University Lucknow], [2022-2025]
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills
+        </h1>
+        <span>
+          [Programming Languages: C Programming, C++ (OPPS), Javascript ], 
+          [Libraries/Frameworks:	ReactJs, Tailwind CSS, NodeJs, HTML, CSS ], 
+          [Tools / Platforms:	VS-Code, Github],  
+          [Databases:	MongoDB, MySQL],
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+        Basic-Skills
+        </h1>
+        <span>
+          [Office Automation: MS-Excel, MS-Word, MS-PowerPoint],
+          [Graphic Design: Canva]
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative [Your Field] solutions that exceed client expectations and
+          contribute positively to the digital landscape. I am committed to
+          continuous learning and growth, always seeking new challenges and
+          opportunities to expand my horizons.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
